3beb1f422ac9692a169b25376a31127f142362a3
shatko <mladen.salaj@gmail.com>
2015-08-19 13:22:15 +0200

    inserts a generic next button

--- templates/factslider.php
+++ templates/factslider.php
@@ -1,6 +1,8 @@
 <body>
-<?php include ('factcontainer/fact_1.php'); ?> 
-
+<?php include ('factcontainer/fact_1.php'); ?>
+<div class="btn">
+    <button type="button" class="btn">Next <span class="desno">(desna strelica)</span></button>
+</div>
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 </body>
