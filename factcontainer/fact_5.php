83489fd2d32013c875deebf3037e096ea8bb2805
shatko <mladen.salaj@gmail.com>
2015-08-19 12:40:56 +0200

    creates 5 test slides

---
+++ factcontainer/fact_5.php
@@ -0,0 +1,14 @@
+
+<div class="container">
+    <div class="row ">
+        <div class="col-md-6 imgholder">
+            <img src="/img/zagreb.jpg" alt="Zagreb">
+            <p></p>
+        </div>
+        <div class="col-md-6">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, voluptatum id. Aperiam debitis reiciendis ex minus hic maxime, ducimus architecto, nisi facere ipsa quae, explicabo accusamus, necessitatibus commodi iste ad?</h2>
+        </div>
+    </div>
+    <a href="https://hr.wikipedia.org/wiki/Dalmatinski_pas" target="_blank">Želim znati više!</a>
+</div>
+
