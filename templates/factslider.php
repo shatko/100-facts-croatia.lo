c4affa335f47531e840104dfcee8807cc59fc6b6
shatko <mladen.salaj@gmail.com>
2015-08-29 17:26:35 +0200

    deletes unnecessary files, styles and cleans the code

--- templates/factslider.php
+++ templates/factslider.php
@@ -1,56 +1,76 @@
 <script type="text/javascript"> 
 </script>
 
+
+
 <body>
+    <?php 
+
+        $sql1 = "SELECT * FROM crofacts";
+        $result1 = $conn->query($sql1);
+
+        $factIdMin = 1;
+        $factIdMax = $result1->num_rows;
+        $factId = $_GET['fact'];
+        
+        if ($factId == NULL) {
+            $factId = rand($factIdMin,$factIdMax);
+        }
+
+        $sql2 = "SELECT * FROM crofacts WHERE croid = $factId";
+        $result2 = $conn->query($sql2);
+
+        while ($row = $result2->fetch_assoc()) {
+            $display_image = $row['croimages'];
+            $display_explained = $row['croexplained'];
+            $display_link = $row['crolink'];
+        }
+
+        $conn->close();
+
+
+    ?>
+
+    <?php 
+
+    function getRandomFactId($randomIdMin, $randomIdMax) {
+        $randomFactId = rand($randomIdMin, $randomIdMax);
+        return $randomFactId;
+    }
+
+    function getNextFactId($currentId, $factIdMaxNext) {
+        $nextFactId = $currentId + 1;
+        if ($nextFactId > $factIdMaxNext) {
+            $nextFactId = 1;
+        }
+        return $nextFactId;
+    }
+
+     ?>
+
     <div class="container">
-        <?php
-
-        $selector = 1;
-
-        $query = "SELECT * FROM crofacts WHERE croid = $selector";
-        
-        if ($is_query_running = mysql_query($query)) {
-            echo "query running !!".'<br>';
-        }
-        else {
-            echo "query NOT running !!";
-        }
-
-        
-        while ($query_execute = mysql_fetch_assoc($is_query_running)){
-            $display_image = ('<img src = "'.$query_execute['croimages'].'"');
-            $display_explained = ($query_execute['croexplained']);
-            $display_link = ($query_execute['crolink']);
-            }
-        ?>
-
-
-
-        <div class="row ">
-            <div class="col-md-6 imgholder">
-                <?php
-                    echo $display_image;
-                ?>
-                <p></p>
+        <div class="facts"> 
+            <div class="row ">
+                <div class="col-md-6 imgholder">
+                    <img src="<?php echo $display_image; ?>" alt="">
+                    <p></p>
+                </div>
+                <div class="col-md-6">
+                    <h2>
+                        <?php
+                            echo $display_explained;
+                        ?>
+                    </h2>
+                </div>
             </div>
-            <div class="col-md-6">
-                <h2>
-                    <?php
-                        echo $display_explained;
-                    ?>
-                </h2>
+            <p> Želim znati više: <a class="link" href="<?php echo $display_link;?>" target=_blank>link!</a></p>
+            <div class="buttons-container">
+                <a class="buttons" href="?fact=<?php echo getNextFactId($factId, $factIdMax); ?>">next</a>
+                <a class="buttons" href="?fact=<?php echo getRandomFactId($factIdMin, $factIdMax); ?>">random</a>
             </div>
         </div>
-        <?php
-            echo $display_link;
-        ?>
     </div>
 
-
-
-    <div class="btn">
-        <button type="button" class="btn">Next <span class="desno">(desna strelica)</span></button>
-    </div>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
