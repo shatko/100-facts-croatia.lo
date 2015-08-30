515346abbfb97944d13ca65f70fd91e758912c40
shatko <mladen.salaj@gmail.com>
2015-08-30 10:53:13 +0200

    styles header and footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,8 @@
 <footer>
-    <h1>Footer</h1>
+<hr>
+<h3><span class="text-white">Like what you see?</br></span></h3>
+<span class="text-white">Keep calm and give me a shoutout!</br></span>
+<a class="twitterlogo" href="https://twitter.com/mladensalaj" target=_blank></a>
+<p>&copy; 2015 Mladen Šalaj<p>  
 </footer>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,3 @@
 <header>
-	<h1>100 facts about CROATIA! :)</h1><p class="small_header">(and maybe a couple more...)</p>
+	<h1><strong>100 facts about CROATIA! :)</strong></h1><p class="small_header">(and maybe a couple more...)</p>
 </header>
