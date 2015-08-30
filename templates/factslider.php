d37c101a97c4c5dfbda1e30f4b084a2552fd0222
shatko <mladen.salaj@gmail.com>
2015-08-30 21:00:28 +0200

    adds prev button fixes indentation

--- templates/factslider.php
+++ templates/factslider.php
@@ -2,54 +2,43 @@
 
 window.addEventListener("keydown", goNext, false); 
 function goNext(e) {
-
     switch(e.keyCode) {
         case 39:
             var buttonNext = document.getElementById('button-next');
             document.location.href = buttonNext.href;
             break;
+        case 37:
+            var buttonPrev = document.getElementById('button-prev');
+            document.location.href = buttonPrev.href;
+            break;
     }   
 }
-
 </script>
-
-
 
 <body>
     <?php 
-
         $sql1 = "SELECT * FROM crofacts";
         $result1 = $conn->query($sql1);
-
         $factIdMin = 1;
         $factIdMax = $result1->num_rows;
         $factId = $_GET['fact'];
-        
         if ($factId == NULL) {
             $factId = rand($factIdMin,$factIdMax);
         }
-
         $sql2 = "SELECT * FROM crofacts WHERE croid = $factId";
         $result2 = $conn->query($sql2);
-
         while ($row = $result2->fetch_assoc()) {
             $display_image = $row['croimages'];
             $display_explained = $row['croexplained'];
             $display_link = $row['crolink'];
         }
-
         $conn->close();
-
-
     ?>
-
     <?php 
-
     function getRandomFactId($randomIdMin, $randomIdMax) {
         $randomFactId = rand($randomIdMin, $randomIdMax);
         return $randomFactId;
     }
-
     function getNextFactId($currentId, $factIdMaxNext) {
         $nextFactId = $currentId + 1;
         if ($nextFactId > $factIdMaxNext) {
@@ -57,9 +46,14 @@ function goNext(e) {
         }
         return $nextFactId;
     }
-
-     ?>
-
+    function getPrevFactId($currentId, $factIdMaxNext) {
+        $prevFactId = $currentId - 1;
+        if ($prevFactId < 1) {
+            $prevFactId = $factIdMaxNext;
+        }
+        return $prevFactId;
+    }
+    ?>
     <div class="container">
         <div class="facts"> 
             <div class="row ">
@@ -75,14 +69,14 @@ function goNext(e) {
                     </h2>
                 </div>
             </div>
-            <p> Želim znati više: <a class="link" href="<?php echo $display_link;?>" target=_blank>link!</a></p>
+            <p> Želim znati više: <a class="linkmore" href="<?php echo $display_link;?>" target=_blank>link!</a></p>
             <div class="buttons-container">
-                <a id="button-next" class="buttons" href="?fact=<?php echo getNextFactId($factId, $factIdMax); ?>">next</a>
-                <a class="buttons" href="?fact=<?php echo getRandomFactId($factIdMin, $factIdMax); ?>">random</a>
+                <a id="button-next" class="buttons" href="?fact=<?php echo getNextFactId($factId, $factIdMax); ?>">sljedeći</a>
+                <a class="buttons" href="?fact=<?php echo getRandomFactId($factIdMin, $factIdMax); ?>">slučajni</a>
+                <a id="button-prev" class="buttons" href="?fact=<?php echo getPrevFactId($factId, $factIdMax); ?>">predhodni</a>
             </div>
         </div>
     </div>
-
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
