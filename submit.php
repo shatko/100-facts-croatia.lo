cf9f456cd3790089055e3bb5550dc04b2cde0fc5
shatko <mladen.salaj@gmail.com>
2015-09-08 08:55:20 +0200

    inserts add image into submit formular

--- submit.php
+++ submit.php
@@ -4,7 +4,6 @@
 
 		$errors = array();
 
-
 		if(empty($_POST['name'])) {
 			$errors['name1'] = "Enter the name of the fact!";
 		}
@@ -14,10 +13,25 @@
 		if(!preg_match('/^(http|https):/', $_POST['link'])) {
 			$errors['link1'] = "Enter valid link!";
 		}
+		// imageloader
+
+		$imagename=$_FILES['file']['name'];
+		$imagesize=$_FILES['file']['size'];
+		$imagetype=$_FILES['file']['type'];
+
+		$tmp_name = $_FILES['file']['tmp_name'];
+
+		$imagelocation = 'img/';
+
+		$target_file = $imagelocation . basename($imagename);
 
 
-
-		var_dump(!preg_match('/^(http|https):./', $_POST['link']));
+		if (empty($imagename)) {
+			echo 'Choose an image!';
+		} else {
+			echo 'An image was uploaded';
+			move_uploaded_file($tmp_name, $target_file);
+		}
 
 		if (count($errors) == 0) {
 			$dbhost = "localhost";
@@ -30,10 +44,12 @@
 				die("Connection failed: " . $conn->connect_error);
 			} 
 			mysqli_select_db($conn, "mladen");
-			$sql = "INSERT INTO crofacts (croname,croexplained,crolink,croimages) VALUES ('$_POST[name]','$_POST[explained]','$_POST[link]','$_POST[images]')";
+			$sql = "INSERT INTO crofacts (croname,croexplained,crolink,croimages) VALUES ('$_POST[name]','$_POST[explained]','$_POST[link]','$imagename')";
 			mysqli_query($conn, $sql);
 			mysqli_close($conn);
 		}
+
+
 	}
 ?>
 
@@ -44,15 +60,19 @@
 	<title>insert</title>
 </head>
 <body>
-	<form action="submit.php" method="post">
-		Name: <input type="text" name="name"></br>
-		<?php if(isset($errors['name1'])) echo '<p>' . $errors['name1'] . '</p>'; ?>
-		Explained: <textarea name="explained" rows="10" cols="100"></textarea></br>
+	<form action="submit.php" method="post" enctype="multipart/form-data">
+
+		Name: <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>" /></br> <?php if(isset($errors['name1'])) echo '<p>' . $errors['name1'] . '</p>'; ?>
+
+		Explained: <textarea name="explained" rows="10" cols="100" /><?php if(isset($_POST['explained'])) echo $_POST['explained']; ?></textarea></br> 
 		<?php if(isset($errors['explained1'])) echo '<p>' . $errors['explained1'] . '</p>'; ?>
-		Link: <input type="text" name="link"></br>
+
+		Link: <input type="text" name="link" value="<?php if(isset($_POST['link'])) echo $_POST['link']; ?>" /></br>
 		<?php if(isset($errors['link1'])) echo '<p>' . $errors['link1'] . '</p>'; ?>
-		Images: <input type="text" name="images"></br>
-		<input type="submit" name="submit">
+
+		<input type="file" name="file" accept="image/*" /></br>
+
+		<input type="submit" name="submit" value="submit">
 	</form>
 </body>
 </html>
