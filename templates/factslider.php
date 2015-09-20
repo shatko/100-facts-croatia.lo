8f5c21d86392ce88e41ee00c39ca94eb2963e1b1
shatko <mladen.salaj@gmail.com>
2015-09-20 22:15:07 +0200

    factslider.php cleanup

--- templates/factslider.php
+++ templates/factslider.php
@@ -16,7 +16,6 @@
 </script>
 <!-- PHP starts -->
 <?php 
-
 // database stuff 
     $sql = "SELECT croid FROM crofacts";
     $data = mysqli_query($conn, $sql);
@@ -24,18 +23,11 @@
     while($row = mysqli_fetch_array($data)){
         $croid_array[] = $row['croid'];
     }
-/* sql1 is needed for the id number of id's */
-    $sql1 = "SELECT * FROM crofacts";
-    $result1 = $conn->query($sql1);
-    $factIdMin = 1;
-    $factIdMax = $result1->num_rows;
 /* shortcode for the if loop so we get a false value on initial entry not a NULL*/ 
     $factId = isset($_GET['fact']) ? $_GET['fact'] : false;
     if ($factId == false) {
         $randomCroid = array_rand($croid_array);
         $factId = $croid_array[$randomCroid];
-        var_dump($factId);
-        echo $factId;
     }
 /* sql2 references to the row by selected id */
     $sql2 = "SELECT * FROM crofacts WHERE croid = $factId";
@@ -55,15 +47,26 @@
     }
 /* increment by one fact, button */
     function getNextFactId($currentId, $EveryCroid) {
-        $currentId = next($EveryCroid);
-        $nextFactId = $EveryCroid[$currentId];
+        reset($EveryCroid);
+        while (!in_array(current($EveryCroid), [$currentId, null])) {
+           next($EveryCroid);
+        }
+        $nextFactId = next($EveryCroid);
+        if ($nextFactId == false) {
+            $nextFactId = reset($EveryCroid);
+        }
         return $nextFactId;
     }
 /* decrement by one fact, button */ 
-    function getPrevFactId($currentId, $factIdMaxNext) {
-        $prevFactId = $currentId - 1;
-        if ($prevFactId < 1) {
-            $prevFactId = $factIdMaxNext;
+    function getPrevFactId($currentId, $EveryCroid) {
+        reset($EveryCroid);
+        while (!in_array(current($EveryCroid), [$currentId, null])) {
+           next($EveryCroid);
+        }
+        $prevFactId = prev($EveryCroid);
+        $EndString = 1;
+        if ($prevFactId < $EndString) {
+            $prevFactId = end($EveryCroid);
         }
         return $prevFactId;
     }
@@ -94,7 +97,7 @@
             <div class="buttons-container">
                 <a id="button-next" class="buttons" href="?fact=<?php echo getNextFactId($factId, $croid_array); ?>">sljedeći &#8594;</a>
                 <a class="buttons" href="?fact=<?php echo getRandomFactId($factId, $croid_array); ?>">slučajni</a>
-                <a id="button-prev" class="buttons" href="?fact=<?php echo getPrevFactId($factId, $factIdMax); ?>">&#8592; predhodni</a>
+                <a id="button-prev" class="buttons" href="?fact=<?php echo getPrevFactId($factId, $croid_array); ?>">&#8592; predhodni</a>
             </div>
         </div>
     </div>
