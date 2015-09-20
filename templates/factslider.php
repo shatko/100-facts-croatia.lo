78527e2ab65e289a05a8c4dbb1f0cace2d65fb01
shatko <mladen.salaj@gmail.com>
2015-09-20 20:06:04 +0200

    sets random button

--- templates/factslider.php
+++ templates/factslider.php
@@ -16,6 +16,14 @@
 </script>
 <!-- PHP starts -->
 <?php 
+
+// database stuff 
+    $sql = "SELECT croid FROM crofacts";
+    $data = mysqli_query($conn, $sql);
+// puts the key  => value of croid column into an array
+    while($row = mysqli_fetch_array($data)){
+        $croid_array[] = $row['croid'];
+    }
 /* sql1 is needed for the id number of id's */
     $sql1 = "SELECT * FROM crofacts";
     $result1 = $conn->query($sql1);
@@ -24,7 +32,10 @@
 /* shortcode for the if loop so we get a false value on initial entry not a NULL*/ 
     $factId = isset($_GET['fact']) ? $_GET['fact'] : false;
     if ($factId == false) {
-        $factId = rand($factIdMin,$factIdMax);
+        $randomCroid = array_rand($croid_array);
+        $factId = $croid_array[$randomCroid];
+        var_dump($factId);
+        echo $factId;
     }
 /* sql2 references to the row by selected id */
     $sql2 = "SELECT * FROM crofacts WHERE croid = $factId";
@@ -37,16 +48,15 @@
     }
     $conn->close();
 /* random select fact button function */ 
-    function getRandomFactId($randomIdMin, $randomIdMax) {
-        $randomFactId = rand($randomIdMin, $randomIdMax);
+    function getRandomFactId($currentId, $EveryCroid) {
+        $currentId = array_rand($EveryCroid);
+        $randomFactId = $EveryCroid[$currentId];
         return $randomFactId;
     }
 /* increment by one fact, button */
-    function getNextFactId($currentId, $factIdMaxNext) {
-        $nextFactId = $currentId + 1;
-        if ($nextFactId > $factIdMaxNext) {
-            $nextFactId = 1;
-        }
+    function getNextFactId($currentId, $EveryCroid) {
+        $currentId = next($EveryCroid);
+        $nextFactId = $EveryCroid[$currentId];
         return $nextFactId;
     }
 /* decrement by one fact, button */ 
@@ -82,8 +92,8 @@
             <p> Želim znati više: <a class="linkmore" href="<?php echo $display_link;?>" target=_blank>link!</a></p>
 <!-- buttons -->
             <div class="buttons-container">
-                <a id="button-next" class="buttons" href="?fact=<?php echo getNextFactId($factId, $factIdMax); ?>">sljedeći &#8594;</a>
-                <a class="buttons" href="?fact=<?php echo getRandomFactId($factIdMin, $factIdMax); ?>">slučajni</a>
+                <a id="button-next" class="buttons" href="?fact=<?php echo getNextFactId($factId, $croid_array); ?>">sljedeći &#8594;</a>
+                <a class="buttons" href="?fact=<?php echo getRandomFactId($factId, $croid_array); ?>">slučajni</a>
                 <a id="button-prev" class="buttons" href="?fact=<?php echo getPrevFactId($factId, $factIdMax); ?>">&#8592; predhodni</a>
             </div>
         </div>
