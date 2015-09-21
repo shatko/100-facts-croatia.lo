5c74b49e3056683cf00ff0b1645158df3f8e1d18
shatko <mladen.salaj@gmail.com>
2015-09-21 10:32:37 +0200

    changes header

--- fun.php
+++ fun.php
@@ -2,7 +2,7 @@
 <?php include ('templates/head.php'); ?> 
 
 <header>
-	<h1><strong>100 facts about CROATIA! <a class="eastersmyle" href="index.php"> *go back*</a></strong></h1><p class="small_header">(and maybe a couple more...)</p>
+	<h1><strong>100 FUN facts about CROATIA! <a class="eastersmyle" href="index.php"> *go back*</a></strong></h1><p class="small_header">(and maybe a couple more...)</p>
 </header>
 
 <?php include ('templates/factslider.php'); ?>
